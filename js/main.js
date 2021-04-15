869197a25b1d20142579d6677ba4c6805ab3549e
Aporve <aporvep@gmail.com>
2021-04-15 10:20:40 +0530

    Updated code

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ let frame = document.getElementById("iframeWrapper");
 let iframeUI = document.getElementById('iframe2');
 let test1 = document.getElementById('close')
 let widget = document.getElementById('vanguard-icon-wrapper');
+let  childWindow = document.getElementsByTagName("iframe")[0].contentWindow;
+let childW = document.getElementById('vanguard-iframe').contentWindow;
+// let childWindow = document.getElementById('vanguard-iframe');
 // const uiFrameWindow = iframeUI.contentWindow;
 // const uiFrameDocument = iframeUI.contentDocument
 // console.error(uiFrameDocument, "8888888888888888888888888888888888")
@@ -29,31 +32,53 @@ test1.addEventListener('click', () => {
 function sendEvent(btnID) {
     let clickedButton = btnID;
     alert(clickedButton);
-    // alert('Send Event', btnID)
-    // var sendingEvent = {}
-    // var raw = JSON.stringify({
-    //     "details": {
-    //     "companyName": "Zalora",
-    //     "user-feedback": document.getElementById('feedback').value
-    //     }
-    // });
+    console.log('Testing');
+
+    // childW.postMessage(JSON.stringify({
+    //     event_code: 'ym-client-event', data: JSON.stringify({
+    //         event: { 
+    //         code: "clickedBtn",
+    //         data: clickedButton
+    //         }
+    //     })
+    // }), '*');
 
     // feedbackDetails.data = raw;
-    window.parent.postMessage(JSON.stringify({
-        event_code: 'ym-client-event', data: JSON.stringify({
-            event: {
-            code: "clickedBtn",
-            data: clickedButton,
-        
+    document.getElementById('vanguard-iframe').contentWindow.postMessage(JSON.stringify({
+        event_code: 'ym-client-event',
+        data: JSON.stringify({
+        event: {
+             code: "recaptcha_token",
+             data: clickedButton
             }
         })
-    }), '*');
+   }), '*');
+   return;
+    // window.parent.postMessage(JSON.stringify({
+    //     event_code: 'ym-client-event', data: JSON.stringify({
+    //         event: {
+    //         code: "clickedBtn",
+    //         data: clickedButton
+    //         }
+    //     })
+    // }), '*');
 
-    // console.error("Feedback Details ---->",feedbackDetails);
+    // HTMLIFrameElement.contentWindow.postMessage(JSON.stringify({
+    //     event_code: 'ym-client-event', data: JSON.stringify({
+    //         event: {
+    //         code: "clickedBtn",
+    //         data: clickedButton
+    //         }
+    //     })
+    // }), '*');
+
+    // childWindow.contentWindow.postMessage(JSON.stringify({
+    //     event_code: 'ym-client-event', data: JSON.stringify({
+    //         event: {
+    //         code: "clickedBtn",
+    //         data: clickedButton
+    //         }
+    //     })
+    // }), '*');
+
 }
-
-
-// function disable() {
-//     widget.style.display = 'none';
-
-// }
